fd968d7322650bb2a58c5f5e1247c0546db12196
Mnlght0312 <enkhmanlai.serious@gmail.com>
2025-05-11 15:50:33 +0800

    Yronhiid ni bol duuslaa

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,38 +13,39 @@ const Contact = () => {
       <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
         <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96">
           <ImageClipBox
-            src="/img/contact-1.webp"
+            src="/img/contact-1.jpg"
             clipClass="contact-clip-path-1"
           />
           <ImageClipBox
-            src="/img/contact-2.webp"
+            src="/img/contact-2.jpg"
             clipClass="contact-clip-path-2 lg:translate-y-40 translate-y-60"
           />
         </div>
 
         <div className="absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80">
-          <ImageClipBox
+          {/* <ImageClipBox
             src="/img/swordman-partial.webp"
             clipClass="absolute md:scale-125"
-          />
+          /> */}
           <ImageClipBox
-            src="/img/swordman.webp"
+            src="/img/swordman.jpg"
             clipClass="sword-man-clip-path md:scale-125"
           />
         </div>
 
-        <div className="flex flex-col items-center text-center">
-          <p className="mb-10 font-general text-[10px] uppercase">
-            Join Zentry
-          </p>
+        <div className="flex flex-col items-center text-center w-full max-w-[90%] sm:max-w-[600px] mx-auto px-4">
+  <p className="mb-10 font-general text-[10px] uppercase">
+    Видеограф, График дизайн, Дижитал маркетинг
+  </p>
 
-          <AnimatedTitle
-            title="let&#39;s b<b>u</b>ild the <br /> new era of <br /> g<b>a</b>ming t<b>o</b>gether."
-            className="special-font !md:text-[6.2rem] w-full font-zentry !text-5xl !font-black !leading-[.9]"
-          />
+  <AnimatedTitle
+    title="Дүрслэл,<br /> Дизайн, <br /> Дижитал маркетинг"
+    className="special-font w-full font-zentry font-black leading-tight text-2xl sm:text-4xl md:text-5xl lg:text-[5rem]"
+  />
 
-          <Button title="contact us" containerClass="mt-10 cursor-pointer" />
-        </div>
+  <Button title="Холбоо барих" containerClass="mt-10 cursor-pointer" />
+</div>
+
       </div>
     </div>
   );
--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -49,12 +49,12 @@ const FloatingImage = () => {
     <div id="story" className="min-h-dvh w-screen bg-black text-blue-50">
       <div className="flex size-full flex-col items-center py-10 pb-24">
         <p className="font-general text-sm uppercase md:text-[10px]">
-          the multiversal ip world
+          Контент бүтээгчдэд зориулсан анги 
         </p>
 
         <div className="relative size-full">
           <AnimatedTitle
-            title="the st<b>o</b>ry of <br /> a hidden real<b>m</b>"
+            title=" C<b>i</b>nematography, <br /> Video Edit<b>o</b>r"
             containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
           />
 
@@ -67,8 +67,8 @@ const FloatingImage = () => {
                   onMouseLeave={handleMouseLeave}
                   onMouseUp={handleMouseLeave}
                   onMouseEnter={handleMouseLeave}
-                  src="/img/entrance.webp"
-                  alt="entrance.webp"
+                  src="/img/entrance.jpg"
+                  alt="entrance.jpg"
                   className="object-contain"
                 />
               </div>
@@ -106,14 +106,13 @@ const FloatingImage = () => {
         <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
           <div className="flex h-full w-fit flex-col items-center md:items-start">
             <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
-              Where realms converge, lies Zentry and the boundless pillar.
-              Discover its secrets and shape your fate amidst infinite
-              opportunities.
+              Видеограф, график дизайн, дижитал маркетингийн чиглэлээр контент үйлдвэрлэх, хөгжүүлэх,
+              засварлах, найруулах, бүтэц зохион байгуулах ур чадвар эзэмшинэ.
             </p>
 
             <Button
               id="realm-btn"
-              title="discover prologue"
+              title="Дэлгэрэнгүй үзэх"
               containerClass="mt-5"
             />
           </div>
